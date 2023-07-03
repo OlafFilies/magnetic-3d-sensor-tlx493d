e9d6324098afc326375a6b175ad75d38758abdca
Monideep Bora <monideep.bora@infineon.com>
2023-07-03 09:22:16 +0200

    added base structures and function prototypes

--- src/sensors/sensors_gen_1/TLE493D_A1B6/TLE493D_A1B6.c
+++ src/sensors/sensors_gen_1/TLE493D_A1B6/TLE493D_A1B6.c
@@ -18,4 +18,46 @@
 #include "TLE493D_A1B6_config.h"
 #include "TLE493D_A1B6.h"
 
+// structures for holding communication params
+extern struct ComLibraryFunctions_ts comLibIF_i2c;
+extern void setI2CParameters(ComLibraryParameters_ts *params); //TODO: assign GEN1 addr 
+extern void frameworkDelayMicroseconds(uint32_t us);
+
+/*
+  Listing of all register names for this sensor.
+  Used to index "TLE493D_A2B6_regDef" defined below, so index values must match !
+*/
+typedef enum {
+               BX_MSB = 0,
+               BY_MSB,
+               BZ_MSB,
+               TEMP_MSB,
+               BX_LSB,
+               BY_LSB,
+               TEMP_LSB,
+               ID,
+               BZ_LSB,
+               P,
+               FF,
+               CF,
+               T,
+               PD3,
+               PD0,
+               FRM,
+               DT,
+               AM,
+               TRIG,
+               X2,
+               TL_mag,
+               CP,
+               FP,
+               IICaddr,
+               PR,
+               CA,
+               INT,
+               MODE,
+               PRD,
+               Type,
+               HWV } TLE493D_A1B6_registerNames_te;
+
 
--- src/sensors/sensors_gen_1/sensors_gen_1_config_common.h
+++ src/sensors/sensors_gen_1/sensors_gen_1_config_common.h
@@ -1,5 +1,7 @@
 #ifndef SENSORS_GEN_1_CONFIG_COMMON_H
 #define SENSORS_GEN_1_CONFIG_COMMON_H
 
+// General defines
+#define GEN_1_STD_IIC_ADDR          0x37 //TODO: verify this 
 
 #endif // SENSORS_GEN_1_CONFIG_COMMON_H
