29cb99356ad323e1abf6e16f2d1a7376dcceff09
Monideep Bora <monideep.bora@infineon.com>
2023-07-04 11:36:44 +0200

    added reg map for TLE_A1B6

--- src/sensors/sensors_gen_1/TLE493D_A1B6/TLE493D_A1B6.c
+++ src/sensors/sensors_gen_1/TLE493D_A1B6/TLE493D_A1B6.c
@@ -1,4 +1,5 @@
 // std includes
+#include <assert.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -20,44 +21,103 @@
 
 // structures for holding communication params
 extern struct ComLibraryFunctions_ts comLibIF_i2c;
+
 extern void setI2CParameters(ComLibraryParameters_ts *params); //TODO: assign GEN1 addr 
 extern void frameworkDelayMicroseconds(uint32_t us);
 
 /*
   Listing of all register names for this sensor.
-  Used to index "TLE493D_A2B6_regDef" defined below, so index values must match !
+  Used to index "TLE493D_A1B6_regDef" defined below, so index values must match !
 */
 typedef enum {
                BX_MSB = 0,
                BY_MSB,
                BZ_MSB,
                TEMP_MSB,
+               FRM,
+               CH,
                BX_LSB,
                BY_LSB,
-               TEMP_LSB,
-               ID,
-               BZ_LSB,
-               P,
+               T_read,
                FF,
-               CF,
-               T,
-               PD3,
-               PD0,
-               FRM,
-               DT,
-               AM,
-               TRIG,
-               X2,
-               TL_mag,
-               CP,
-               FP,
+               PD,
+               BZ_LSB,
+               TEMP_LSB,
+               P,
                IICaddr,
-               PR,
-               CA,
                INT,
-               MODE,
-               PRD,
-               Type,
-               HWV } TLE493D_A1B6_registerNames_te;
+               FAST,
+               LOW,
+               T_write, // remove: ask about duplicates
+               LP,
+               PT } TLE493D_A1B6_registerNames_te;
 
 
+Register_ts TLE493D_A1B6_regDef[TLE493D_A1B6_REGISTER_MAP_SIZE] = {
+    // Read registers
+    { BX_MSB, READ_MODE_e, 0x00, 0xFF, 0 },
+    { BY_MSB, READ_MODE_e, 0x01, 0xFF, 0 },
+    { BZ_MSB, READ_MODE_e, 0x02, 0xFF, 0 },
+    { TEMP_MSB, READ_MODE_e, 0x03, 0xF0, 4 },
+    { FRM, READ_MODE_e, 0x03, 0x0C, 2},
+    { CH, READ_MODE_e, 0x03, 0x03, 0 },
+    { BX_LSB, READ_MODE_e, 0x04, 0xF0, 4 },
+    { BY_LSB, READ_MODE_e, 0x04, 0x0F, 0 },
+    { BX_MSB, READ_MODE_e, 0x00, 0xFF, 0 },
+    { T_read, READ_MODE_e, 0x05, 0x40, 6 },
+    { FF, READ_MODE_e, 0x05, 0x20, 5 },
+    { PD, READ_MODE_e, 0x05, 0x10, 4 },
+    { BZ_LSB, READ_MODE_e, 0x05, 0x0F, 0 },
+    { TEMP_LSB, READ_MODE_e, 0x06, 0xFF, 0 },
+    // Write Registers
+    { P, WRITE_MODE_e, 0x01, 0x80, 7 },
+    { IICaddr, WRITE_MODE_e, 0x01, 0x60, 5 },
+    { INT, WRITE_MODE_e, 0x01, 0x04, 2 },
+    { FAST, WRITE_MODE_e, 0x01, 0x02, 1 },
+    { LOW, WRITE_MODE_e, 0x01, 0x01, 0 },
+    { T_write, WRITE_MODE_e, 0x03, 0x80, 7 },
+    { LP, WRITE_MODE_e, 0x03, 0x40, 6 },
+    { PT, WRITE_MODE_e, 0x03, 0x20, 5 },
+};
+
+CommonFunctions_ts TLE493D_A1B6_commonFunctions = {
+                                .init                  = TLE493D_A1B6_init,
+                                .deinit                = TLE493D_A1B6_deinit,
+
+                                .getTemperature        = TLE493D_A1B6_getTemperature,
+                                .updateGetTemperature  = TLE493D_A1B6_updateGetTemperature,
+
+                                .getFieldValues        = TLE493D_A1B6_getFieldValues,
+                                .updateGetFieldValues  = TLE493D_A1B6_updateGetFieldValues,
+
+                                .reset                 = TLE493D_A1B6_reset,
+                                .getDiagnosis          = TLE493D_A1B6_getDiagnosis,
+                                .calculateParity       = TLE493D_A1B6_calculateParity,
+
+                                .setDefaultConfig      = TLE493D_A1B6_setDefaultConfig,
+                                .updateRegisterMap     = TLE493D_A1B6_updateRegisterMap,
+                              };
+
+
+bool TLE493D_A1B6_init(Sensor_ts *sensor, SupportedComLibraryInterfaceTypes_te comLibIF) {
+    // This sensor only supports I2C.
+    if( comLibIF != I2C_e ) {
+        assert(0);
+        return false;
+    }
+
+    sensor->regMap     = (uint8_t *) malloc(sizeof(uint8_t) * TLE493D_A1B6_REGISTER_MAP_SIZE);
+    sensor->regDef     = TLE493D_A1B6_regDef;
+    sensor->functions  = &TLE493D_A1B6_commonFunctions;
+    sensor->regMapSize = TLE493D_A1B6_REGISTER_MAP_SIZE;
+    sensor->sensorType = TLE493D_A1B6_e;
+    sensor->comIFType  = comLibIF;
+    sensor->comLibIF   = &comLibIF_i2c;
+    sensor->comLibObj  = NULL;
+
+    setI2CParameters(&sensor->comLibIFParams); //remove: this sets GEN2 addr
+
+    sensor->comLibIFParams.i2c_params.address = GEN_1_STD_IIC_ADDR; //remove: ask about this override
+
+    return true;
+}
--- src/sensors/sensors_gen_1/sensors_gen_1_config_common.h
+++ src/sensors/sensors_gen_1/sensors_gen_1_config_common.h
@@ -2,6 +2,6 @@
 #define SENSORS_GEN_1_CONFIG_COMMON_H
 
 // General defines
-#define GEN_1_STD_IIC_ADDR          0x37 //TODO: verify this 
+#define GEN_1_STD_IIC_ADDR          0x1F //remove: this is then <<1 and concat'd with 1/0 
 
 #endif // SENSORS_GEN_1_CONFIG_COMMON_H
