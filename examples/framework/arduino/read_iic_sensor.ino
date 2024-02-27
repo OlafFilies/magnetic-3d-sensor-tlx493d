2c2441bc4075336f897cc5c277b158ae96d0cdcb
Jens Bargfrede <Jens.Bargfrede@infineon.com>
2024-02-27 12:26:31 +0100

    Changed examples for testing.

--- examples/framework/arduino/read_iic_sensor.ino
+++ examples/framework/arduino/read_iic_sensor.ino
@@ -14,13 +14,16 @@ const uint8_t POWER_PIN = LED2;
  */
 // TLx493D_A1B6 dut(Wire, TLx493D_IIC_ADDR_A0_e);
 
-// TLx493D_A2B6 dut(Wire, TLx493D_IIC_ADDR_A0_e);
-TLx493D_P2B6 dut(Wire, TLx493D_IIC_ADDR_A0_e);
+TLx493D_A2B6 dut(Wire, TLx493D_IIC_ADDR_A0_e);
+// TLx493D_P2B6 dut(Wire, TLx493D_IIC_ADDR_A0_e);
 // TLx493D_W2B6 dut(Wire, TLx493D_IIC_ADDR_A0_e);
 // TLx493D_W2BW dut(Wire, TLx493D_IIC_ADDR_A0_e);
 
 // TLx493D_P3B6 dut(Wire, TLx493D_IIC_ADDR_A0_e);
 // TLx493D_P3B6 dut(Wire, TLx493D_IIC_ADDR_A1_e);
+
+
+uint8_t count = 0;
 
 
 void setup() {
@@ -39,29 +42,78 @@ void setup() {
  *  the serial monitor
  */
 void loop() {
-    double temp = 0.0;
-    double valX = 0, valY = 0, valZ = 0;
+    // double temp = 0.0;
+    // double valX = 0, valY = 0, valZ = 0;
 
-    Serial.print(true == dut.getTemperature(&temp) ? "getTemperature ok\n" : "getTemperature error\n");
+    // Serial.print(true == dut.getTemperature(&temp) ? "getTemperature ok\n" : "getTemperature error\n");
+
+    // Serial.print("Temperature is: ");
+    // Serial.print(temp);
+    // Serial.println("°C");
+
+    // Serial.print(true == dut.getMagneticField(&valX, &valY, &valZ) ? "getMagneticField ok\n" : "getMagneticField error\n");
+
+    // Serial.print("Value X is: ");
+    // Serial.print(valX);
+    // Serial.println(" mT");
+    // Serial.print("Value Y is: ");
+    // Serial.print(valY);
+    // Serial.println(" mT");
+    // Serial.print("Value Z is: ");
+    // Serial.print(valZ);
+    // Serial.println(" mT");
+
+    // dut.printRegisters();
+    // Serial.print("\n");
+
+    double t, x, y, z;
+
+    dut.setSensitivity(TLx493D_FULL_RANGE_e);
+    dut.getMagneticFieldAndTemperature(&x, &y, &z, &t);
+    dut.printRegisters();
 
     Serial.print("Temperature is: ");
-    Serial.print(temp);
+    Serial.print(t);
     Serial.println("°C");
 
-    Serial.print(true == dut.getMagneticField(&valX, &valY, &valZ) ? "getMagneticField ok\n" : "getMagneticField error\n");
-
     Serial.print("Value X is: ");
-    Serial.print(valX);
+    Serial.print(x);
     Serial.println(" mT");
     Serial.print("Value Y is: ");
-    Serial.print(valY);
+    Serial.print(y);
     Serial.println(" mT");
     Serial.print("Value Z is: ");
-    Serial.print(valZ);
+    Serial.print(z);
     Serial.println(" mT");
 
+    dut.setSensitivity(TLx493D_SHORT_RANGE_e);
+    dut.getMagneticFieldAndTemperature(&x, &y, &z, &t);
     dut.printRegisters();
-    Serial.print("\n");
+
+    Serial.print("Temperature is: ");
+    Serial.print(t);
+    Serial.println("°C");
+
+    Serial.print("Value X is: ");
+    Serial.print(x);
+    Serial.println(" mT");
+    Serial.print("Value Y is: ");
+    Serial.print(y);
+    Serial.println(" mT");
+    Serial.print("Value Z is: ");
+    Serial.print(z);
+    Serial.println(" mT");
+    Serial.print("\n\n\n\n");
 
     delay(1000);
+
+    Serial.print("count : ");
+    Serial.println(count);
+
+    if( ++count == 10 ) {
+        Serial.println("Before reset -------------------------------------------------------------------------------------------------------");
+        dut.reset();
+        Serial.println("After reset -------------------------------------------------------------------------------------------------------");
+        count = 0;
+    }
 }
--- examples/framework/arduino/read_spi_sensor.ino
+++ examples/framework/arduino/read_spi_sensor.ino
@@ -11,6 +11,8 @@ const uint8_t CHIP_SELECT_PIN = 3;
 
 TLx493D_P3I8 dut(SPI);
 
+uint8_t count = 0;
+
 
 void setup() {
     delay(3000);
@@ -63,4 +65,15 @@ void loop() {
     Serial.print("\n");
 
     delay(1000);
+
+    Serial.print("count : ");
+    Serial.println(count);
+
+    if( ++count == 10 ) {
+        Serial.println("Before reset -------------------------------------------------------------------------------------------------------");
+        // dut.reset();
+        dut.softwareReset();
+        Serial.println("After reset -------------------------------------------------------------------------------------------------------");
+        count = 0;
+    }
 }
